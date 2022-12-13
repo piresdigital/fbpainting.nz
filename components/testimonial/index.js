7e187845072bc028295a833a46c9dfc895e83255
Dhavidy Pires <dhavidy.pires@outlook.com>
2022-12-14 11:36:32 +1200

    update testimonial component to display as slider

--- components/testimonial/index.js
+++ components/testimonial/index.js
@@ -1,31 +1,57 @@
+import { useState } from 'react';
+import { reviews } from '../../data/data';
+
 export default function Testimonial({ children, name, profile, pic, variant }) {
+  const [current, setCurrent] = useState(0);
+  const count = reviews.length;
+
+  const updateCurrent = () => {
+    if (current < count - 1) {
+      setCurrent(current + 1);
+    } else {
+      setCurrent(0);
+    }
+  };
+
+  setTimeout(updateCurrent, 3000);
+
   return (
     <>
       <div
-        className={`flex flex-col w-[85%] mx-auto rounded-lg overflow-hidden ${
+        className={`flex justify-center items-center ${
           variant ? '-translate-y-[6rem]' : ''
-        } border-[6px] border-white shadow-2xl bg-gray-50
-        md:transition-transform md:flex-row md:max-w-[800px] md:hover:scale-105`}>
-        <div
-          className={
-            'w-full h-[200px] overflow-hidden relative z-20 md:flex-1 md:h-[290px]'
-          }>
-          <img src={pic} alt='Client Name' />
-        </div>
-        <div className='pt-5 pb-5 px-5 relative md:flex-1'>
-          <span className='block absolute -top-[40px] -left-[40px] w-[130px] h-[130px] bg-[url(/images/quotes.gif)] bg-cover bg-no-repeat z-0'></span>
+        } md:hover:scale-105`}>
+        <div className='relative w-10/12 max-w-[900px] h-[480px] md:h-[250px]'>
+          {reviews.map((review, index) => {
+            return (
+              <div
+                key={index}
+                className='absolute top-0 left-0 flex flex-col w-full rounded-lg border-[6px] border-white shadow-2xl bg-gray-50 overflow-hidden md:flex-row transition-all duration-1000 opacity-0 translate-y-4'
+                style={
+                  index === current ? { opacity: 1, transform: 'translateY(0)' } : {}
+                }>
+                <div
+                  className='w-full h-[200px] overflow-hidden relative z-20 md:flex-1 md:h-[290px] bg-cover bg-center bg-no-repeat'
+                  style={{ backgroundImage: `url(${review.project})` }}></div>
+                <div className='pt-5 pb-5 px-5 relative md:flex-1'>
+                  <span className='block absolute -top-[40px] -left-[40px] w-[130px] h-[130px] bg-[url(/images/quotes.gif)] bg-cover bg-no-repeat z-0 opacity-70'></span>
 
-          <div className='relative z-10'>
-            <p className='text-xl pt-4 pb-10 indent-16'>{children}</p>
-          </div>
-          <div className='text-gray-500 text-right'>
-            <span>{name}</span>
-            <img
-              className='inline w-12 h-12 rounded-full ml-4'
-              src={profile}
-              alt='client'
-            />
-          </div>
+                  <div className='relative z-10'>
+                    <p className='text-xl pt-4 pb-10 indent-16'>{review.description}</p>
+                  </div>
+                  <div className='text-gray-500 text-right'>
+                    <span>{review.name}</span>
+                    <img
+                      className='inline w-12 h-12 rounded-full ml-4'
+                      src={review.profile}
+                      alt={review.name}
+                    />
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+          {` `}
         </div>
       </div>
     </>
